e36872f13d01bac8f85f920596a9e847a389254f
EC2 Default User <ec2-user@ip-172-31-26-113.ap-northeast-1.compute.internal>
2022-08-17 13:17:23 +0000

    [Update]bookers2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,22 @@
+class BooksController < ApplicationController
+  def new
+    @book_image = Book.new
+  end
+  
+  def create
+    @book_image = Book.new(book_image_params)
+    @book_image.user_id = current_user.id
+    @book_image.save
+    redirect_to book_images_path
+  end
+
+  def index
+  end
+
+  def show
+  end
+  
+  private
+  def book_image_params
+    params.require(:book_image).permit(:title, :body)
+end
