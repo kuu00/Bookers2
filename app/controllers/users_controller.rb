01f66b775c8be3dd41cb2c187dfcf03e6a9ec3af
EC2 Default User <ec2-user@ip-172-31-26-113.ap-northeast-1.compute.internal>
2022-09-24 15:35:08 +0000

    [Update]Bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.all
+    @post_images = @user.post_images
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :books, only: [:new, :index, :show, :create]
+  resources :post_images, only: [:new, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
