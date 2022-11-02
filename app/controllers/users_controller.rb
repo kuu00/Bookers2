ad594f8a810b65c4ba4263450aab92e68747e8cc
EC2 Default User <ec2-user@ip-172-31-26-113.ap-northeast-1.compute.internal>
2022-11-02 02:29:45 +0000

    [Update]Bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    user.save
-    redirect_to 'user_path(@user)'
+    @user = User.new(user_params)
+    @user.user_id = current_user.id
+    if @user.save
+      redirect_to user_path
+    else
+      render :edit
+      @user = User.find(params[:id])
+    end
   end
 
   def show
@@ -19,13 +24,10 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.new(user_params)
-    if user.save
-      redirect_to 'user_path(@user)'
-    else
-      @user = User.find(params[:id])
-      render :edit
-    end
+    user = User.find(params[:id])
+    user.update(user_params)
+    flash[:notice] = "You have updated user successfully."
+    redirect_to user_path(user.id)
   end
 
   def index
